d650c4a8f4d5373bab1b9f562e2fcf302bf51e8a
simonpedro <simonpedro.est@gmail.com>
2016-04-10 20:45:16 -0300

    Add GSON and activity main layout
    Se añade GSON y la primera version de la interfaz para buscar ciudades.
    Se posterga el uso de un mapa.

--- app/src/main/java/com/android/sqsoft/sunshine/SearchCityActivity.java
+++ app/src/main/java/com/android/sqsoft/sunshine/SearchCityActivity.java
@@ -2,12 +2,20 @@ package com.android.sqsoft.sunshine;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.gson.Gson;
+
+public class SearchCityActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void mostrarPronostico(View view) {
+
     }
 }
---
+++ app/src/main/java/com/android/sqsoft/sunshine/entities/DayForecastEntity.java
@@ -0,0 +1,8 @@
+package com.android.sqsoft.sunshine.entities;
+
+/**
+ * Created by Pedro on 10/04/2016.
+ */
+public class DayForecastEntity {
+
+}
