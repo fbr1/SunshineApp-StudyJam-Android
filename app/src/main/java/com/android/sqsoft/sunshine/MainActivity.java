e7dd056e4203b0209522a01fd62c8ecf45243689
fbr1 <fbr1@users.noreply.github.com>
2016-04-12 11:30:49 -0300

    added basic listview NOT WORKING

--- app/src/main/java/com/android/sqsoft/sunshine/MainActivity.java
+++ app/src/main/java/com/android/sqsoft/sunshine/MainActivity.java
@@ -2,18 +2,12 @@ package com.android.sqsoft.sunshine;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-public class SearchCityActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_searchcity);
-    }
-
-
-    public void mostrarPronostico(View view) {
-
+        setContentView(R.layout.activity_main);
     }
 }
