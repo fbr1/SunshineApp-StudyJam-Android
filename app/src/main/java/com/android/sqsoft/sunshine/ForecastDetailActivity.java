57a4372632c8ea3ccf4d2b17056de417f47535ea
fbr1 <fbr1@users.noreply.github.com>
2016-04-20 00:16:58 -0300

    add actionbar to detailActivity

--- app/src/main/java/com/android/sqsoft/sunshine/ForecastDetailActivity.java
+++ app/src/main/java/com/android/sqsoft/sunshine/ForecastDetailActivity.java
@@ -2,7 +2,7 @@ package com.android.sqsoft.sunshine;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.RelativeLayout;
+import android.support.v7.widget.Toolbar;
 import android.widget.TextView;
 
 import com.android.sqsoft.sunshine.entities.DayForecast;
@@ -15,10 +15,18 @@ public class ForecastDetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_forecast_city_detail);
         DayForecast dayForecast =(DayForecast) getIntent().getSerializableExtra(getString(R.string.day_forecast_for_intent_key));
 
-        TextView textView = new TextView(this);
-        textView.setText(String.valueOf(dayForecast.getTmin()));
+        TextView textView =  (TextView) findViewById(R.id.tv_Detail);
+        textView.setText("Minimum Temperature: " + String.valueOf(dayForecast.getTmin()));
 
-        RelativeLayout rl =  (RelativeLayout) findViewById(R.id.container);
-        rl.addView(textView);
+        String location = Utility.getLocationName(this);
+
+        // Setup Toolbar
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        if (myToolbar != null) {
+            myToolbar.setTitle(location + " - Detalle");
+            setSupportActionBar(myToolbar);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
     }
 }
