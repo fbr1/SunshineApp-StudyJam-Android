a4e358b8e9b724a44ab5bcc6f5758978ae0a19ad
fbr1 <fbr1@users.noreply.github.com>
2016-04-22 14:09:34 -0300

    change default city and cleanup

--- app/src/main/java/com/android/sqsoft/sunshine/logic/Logic.java
+++ app/src/main/java/com/android/sqsoft/sunshine/logic/Logic.java
@@ -6,9 +6,6 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
 
-/**
- * Created by Pedro on 18/04/2016.
- */
 public class Logic {
     protected final Context context;
     public RequestQueue requestQueue;
